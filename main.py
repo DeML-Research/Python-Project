67d1bf666de145163b89abac8be81315f989bcb8
IFFranciscoME <if.francisco.me@gmail.com>
2021-01-16 12:46:54 -0600

    minimal examples of verification

--- main.py
+++ main.py
@@ -8,3 +8,15 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+import pandas as pd
+import data as dt
+
+# -- TEST 1 : 
+# verify that the script is being read
+print(dt.dict_test)
+
+# -- TEST 2 :
+# verify that installed pandas modeule works correctly
+df_dict_test = pd.DataFrame(dt.dict_test, index=[0,1])
+print(df_dict_test)
