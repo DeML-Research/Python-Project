791d1ee95fa7d7f534ad4e6e7c65ebc906665dd1
IFFranciscoME <if.francisco.me@gmail.com>
2021-01-24 12:27:47 -0600

    add a TEST 4 in jupyter notebook and main.py

--- main.py
+++ main.py
@@ -18,5 +18,31 @@ print(dt.dict_test)
 
 # -- TEST 2 :
 # verify that installed pandas module works correctly
-df_dict_test = pd.DataFrame(dt.dict_test, index=[0,1])
+df_dict_test = pd.DataFrame(dt.dict_test, index=[0, 1])
 print(df_dict_test)
+
+# -- TEST 3 :
+# verify you can use plotly and visualize plots in jupyter notebook
+
+import chart_studio.plotly as py   # various tools (jupyter offline print)
+import plotly.graph_objects as go  # plotting engine
+
+# example data
+df = pd.DataFrame({'column_a': [1, 2, 3, 4, 5], 'column_b': [1, 2, 3, 4, 5]})
+# basic plotly plot
+data = [go.Bar(x=df['column_a'], y=df['column_b'])]
+# instruction to view it inside jupyter
+py.iplot(data, filename='jupyter-basic_bar')
+# (alternatively) instruction to view it in web app of plotly
+# py.plot(data)
+
+# -- TEST 4 :
+# verify you can use plotly and visualize plots in web browser locally
+
+import plotly.io as pio            # to define input-output of plots
+pio.renderers.default = "browser"  # to render the plot locally in your default web browser
+
+# basic plotly plot
+plot_data = go.Figure(go.Bar(x=df['column_a'], y=df['column_b']))
+# instruction to view it in specified render (in this case browser)
+plot_data.show()
